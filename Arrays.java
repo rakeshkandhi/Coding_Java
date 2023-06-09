bb1e6a08d510333c275b5dea3baf0e0b59d74cbb
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-06-09 18:21:46 +0530

    modified:   Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,10 +1,35 @@
 package dsa;
 
-public class Arrays {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.*;
+
+class Arrays {
+	public static int search(int arr[], int N, int X) {
+//		Function for searching the element in the array
+
+		for (int i = 0; i < N; i++) {
+			if (arr[i] == X)
+				return i;
+		}
+		return -1;
 
 	}
 
+	public static void main(String args[]) throws IOException {
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(read.readLine());
+
+		while (t-- > 0) {
+			int sizeOfArray = Integer.parseInt(read.readLine());
+			int arr[] = new int[sizeOfArray];
+
+			String st[] = read.readLine().trim().split("\\s+");
+			for (int i = 0; i < sizeOfArray; i++)
+				arr[i] = Integer.parseInt(st[i]);
+
+			int x = Integer.parseInt(read.readLine());
+
+			System.out.println(search(arr, sizeOfArray, x));
+		}
+	}
 }
