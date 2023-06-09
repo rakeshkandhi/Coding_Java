b615a81c4d1bbfaad780a7aee34b58f57ef01bfc
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-06-09 18:40:06 +0530

    modified:   Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,6 +1,5 @@
 package dsa;
 
-
 import java.io.*;
 
 class Arrays {
@@ -18,17 +17,25 @@ class Arrays {
 	public static void main(String args[]) throws IOException {
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the number of test cases:");
-		int t = Integer.parseInt(read.readLine());
+		int t = 1;
+		
+//		If you have more than 1 test cases uncomment the below line
+		t = Integer.parseInt(read.readLine());
 
 		while (t-- > 0) {
+
 			System.out.println("Enter the size of array:");
 			int sizeOfArray = Integer.parseInt(read.readLine());
+
 			int arr[] = new int[sizeOfArray];
+
 			System.out.println("Enter the array elements seperated by spaces:");
 			String st[] = read.readLine().trim().split("\\s+");
+
 			for (int i = 0; i < sizeOfArray; i++)
 				arr[i] = Integer.parseInt(st[i]);
-System.out.println("Enter the search element:");
+
+			System.out.println("Enter the search element:");
 			int x = Integer.parseInt(read.readLine());
 
 			System.out.println(search(arr, sizeOfArray, x));
