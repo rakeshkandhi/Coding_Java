8c855d7fba8c6c007c7827209fd0a60e038fb7c8
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-06-09 18:34:24 +0530

    modified:   Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -17,16 +17,18 @@ class Arrays {
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number of test cases:");
 		int t = Integer.parseInt(read.readLine());
 
 		while (t-- > 0) {
+			System.out.println("Enter the size of array:");
 			int sizeOfArray = Integer.parseInt(read.readLine());
 			int arr[] = new int[sizeOfArray];
-
+			System.out.println("Enter the array elements seperated by spaces:");
 			String st[] = read.readLine().trim().split("\\s+");
 			for (int i = 0; i < sizeOfArray; i++)
 				arr[i] = Integer.parseInt(st[i]);
-
+System.out.println("Enter the search element:");
 			int x = Integer.parseInt(read.readLine());
 
 			System.out.println(search(arr, sizeOfArray, x));
